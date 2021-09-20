9f456ce5d06408c637676e7b811875c0e7d45afd
yoav <yoatzmona@gmail.com>
2021-09-20 12:18:35 +0300

    new commit

--- src/App/index.js
+++ src/App/index.js
@@ -9,7 +9,7 @@ import Synth from '../audioTrack/synth.mp3'
 import Tanggu from '../audioTrack/tanggu.mp3'
 import { Howl, Howler } from 'howler';
 import React, { useEffect, useState } from 'react'
-import { ButtonGroup, IconButton, makeStyles, Radio, RadioGroup, Typography, ThemeProvider, Paper } from '@material-ui/core'
+import { Switch, ButtonGroup, IconButton, makeStyles, Radio, RadioGroup, Typography, ThemeProvider, Paper, Button } from '@material-ui/core'
 import PlayCircleFilledWhiteOutlinedIcon from '@material-ui/icons/PlayCircleFilledWhiteOutlined';
 import StopOutlinedIcon from '@material-ui/icons/StopOutlined';
 import { grey, red, purple } from '@material-ui/core/colors'
@@ -24,7 +24,7 @@ const theme = createTheme({
         },
         type: "dark",
         primary: {
-        main: '#fefefe'
+            main: '#fefefe'
         }
     },
 
@@ -57,10 +57,22 @@ const useStyles = makeStyles((theme) => {
         sound: {
             fontFamily: 'fantasy',
             margin: theme.spacing(2, 12.5),
-            fontSize: "x-large"
+            fontSize: "x-large",
+        },
+        playRecord: {
+            fontFamily: 'fantasy',
+            margin: theme.spacing(2, 12.5),
+            fontSize: "x-large",
+            '&:hover': {
+                color: red[400]
+            }
         },
         button: {
             margin: "30px"
+        },
+        session: {
+            backgroundColor: "#ffffffba",
+            borderRadius: '30px'
         }
     }
 })
@@ -81,16 +93,25 @@ export default function App() {
     const classes = useStyles(),
         [onAndOff, setOnAndOff] = useState([]),
         [mode, setMode] = useState(''),
-        [counter, setCounter] = useState(0)
+        [counter, setCounter] = useState(0),
+        [checked, setChecked] = useState(false),
+        [session, setSession] = useState(''),
+        [error, setError] = useState(''),
+        [replay, setReplay] = useState([]),
+        [replayMode, setReplayMode] = useState('')
+    // [timer,setTimer] = useState(0)
+
 
 
     function trackPlay(status, arr) {
         if (mode == 'play' && !status) {
-            onAndOff.forEach(item => item.pause())
+            console.log(onAndOff);
+            onAndOff.forEach(item => item.stop())
             onAndOff.forEach(item => item.play())
             setCounter(counter + 1)
         }
         else if (mode == 'join' && !status) {
+            console.log(`join:`, onAndOff)
             onAndOff[0].on('end', function () {
                 setMode('play')
             })
@@ -102,6 +123,7 @@ export default function App() {
         else {
             onAndOff.forEach(item => item.stop())
             setCounter(0)
+            // setOnAndOff([])
         }
     }
 
@@ -112,58 +134,211 @@ export default function App() {
                 loop: true
             })])
             setMode('join')
+            if (checked) {
+                let arr = []
+                arr = JSON.parse(localStorage.record)
+                arr.push({ click: src, time: Date.now(), type: status })
+                localStorage.record = JSON.stringify(arr)
+            }
         }
-        else {
-            let arr = onAndOff.filter(item => item._src != src)
-            trackPlay('off', arr)
-            setOnAndOff(arr)
+            else {
+                let arr = onAndOff.filter(item => item._src != src)
+                trackPlay('off', arr)
+                setOnAndOff(arr)
+            }
         }
-    }
 
-    useEffect(() => trackPlay(), [mode])
+        function rePlay(status, arr) {
+            console.log(status);
+            if (replayMode == 'play' && !status) {
+                console.log(`play:`,replay);
+                replay.forEach(item => item.stop())
+                replay.forEach(item => item.play())
+                setCounter(counter + 1)
+            }
+            else if (replayMode == 'join' && !status) {
+                console.log(`join:`,replay);
+                replay[0].on('end', function () {
+                    setReplayMode('play')
+                })
+            }
+            else if (status == 'off' && counter > 0) {
+                replay.forEach(item => item.pause())
+                arr.forEach(item => item.play())
+            }
+            else if(replayMode == 'stop'){
+                console.log('we got to the stop!');
+                console.log(`replay:`, replay)
+                replay.forEach(item => item.stop())
+                setCounter(0)
+            }
+        }
 
-    Howler.volume(0.2)
+        function handleOnReplay(src, status) {
+            if (status == 'on') {
+                console.log(`src :`,src);
+                setReplay([...replay, new Howl({
+                    src,
+                    loop: true
+                })])
+                setReplayMode('join')
+            }
+            else {
+                let arr = replay.filter(item => item._src != src)
+                rePlay('off', arr)
+                setReplay(arr)
+            }
+        }
 
-    return <ThemeProvider theme={theme}>
-        <Paper className={classes.wrapper}>
-            <div className="container">
-                <Typography
-                    className={classes.title}
-                    variant="h4"
-                >LOOPER MACHINE</Typography>
-                {audioTracks.map((track, index) =>
-                    <div className="loop">
-                        <RadioGroup className={classes.radio} color="primary">
-                            <Radio key={index} value={'on'} className={classes.on} onClick={() => handleOnClick(track.sound, 'on')} /><p className="p">ON</p>
-                            <Radio key={track.label} color="primary" value={'off'} className={classes.r} onClick={() => handleOnClick(track.sound, 'off')} /><p className="p"> OFF</p>
-                        </RadioGroup>
-                        <div className='title'>
-                            <Typography className={classes.sound}>  {track.label}</Typography>
-                            {/* <img className="img" src={track.icon} /> */}
+        function handleRec(event) {
+            
+            setChecked(event.target.checked)
+            if (event.target.checked) {
+                delete localStorage.record
+                if (counter > 0 || onAndOff.length == 0) {
+                    setError(`for record you have to select one track at least or stop the loop`)
+                    setChecked(false)
+                }
+                else {
+                    setError('')
+                    setMode('play')
+                }
+                
+                let arr = []
+                localStorage.record = JSON.stringify([{ start_time: Date.now() }])
+                onAndOff.forEach(item => {
+                    if (onAndOff.length == 1) {
+                        arr = JSON.parse(localStorage.record)
+                        arr.push({ click: item._src }, { click: 'play' })
+                        localStorage.record = JSON.stringify(arr)
+                    }
+                    else {
+                        if (arr.length == onAndOff.length) {
+                            arr = JSON.parse(localStorage.record)
+                            arr.push({ click: item._src }, { click: 'play' })
+                            localStorage.record = JSON.stringify(arr)
+                        }
+                        else {
+                            arr = JSON.parse(localStorage.record)
+                            arr.push({ click: item._src })
+                            localStorage.record = JSON.stringify(arr)
+                        }
+                    }
+                })
+
+            }
+            else {
+                setSession('session')
+            }
+        }
+
+
+        function playSession() {
+            let arr = JSON.parse(localStorage.record)
+            arr.forEach( (item, index)=> {
+                if (index != 0 && item.click != 'play' && item.click != 'stop') {
+                    if (!item.time)
+                        handleOnReplay(item.click, 'on')
+                    else {
+                        setTimeout(() => {
+                            handleOnReplay(item.click, item.type)
+                        }, item.time - arr[0].start_time)
+                    }
+                }
+                else if (item.click == 'play') {
+                    if (!item.time)
+                    setTimeout(() => {
+                        setReplayMode('play')
+                    },0.01)
+                    else {
+                        setTimeout(() => {
+                            setReplayMode('play')
+                        }, item.time - arr[0].start_time)
+                    }
+                }
+                else if (item.click == 'stop') {
+                    setTimeout(() => {
+                        setReplayMode('stop')
+                    }, item.time - arr[0].start_time)
+
+                }
+            })
+        }
+
+        useEffect(() => trackPlay(), [mode])
+        useEffect(() => rePlay(), [replayMode])
+
+        Howler.volume(0.2)
+
+        return <ThemeProvider theme={theme}>
+            <Paper className={classes.wrapper}>
+                <div className="container">
+                    <Typography
+                        className={classes.title}
+                        variant="h4"
+                    >LOOPER MACHINE</Typography>
+                    {audioTracks.map((track, index) =>
+                        <div className="loop">
+                            <RadioGroup className={classes.radio} color="primary">
+                                <Radio key={index} value={'on'} className={classes.on} onClick={() => handleOnClick(track.sound, 'on')} /><p className="p">ON</p>
+                                <Radio key={track.label} color="primary" value={'off'} className={classes.r} onClick={() => handleOnClick(track.sound, 'off')} /><p className="p"> OFF</p>
+                            </RadioGroup>
+                            <div className='title'>
+                                <Typography className={classes.sound}>  {track.label}</Typography>
+                                {/* <img className="img" src={track.icon} /> */}
+                            </div>
                         </div>
-                    </div>
 
-                )}
-                <ButtonGroup className={classes.button}>
-                    <IconButton
-                        className={classes.play}
-                        name="play"
-                        onClick={() => setMode('play')}
+                    )}
+                    <ButtonGroup className={classes.button}>
+                        <IconButton
+                            className={classes.play}
+                            name="play"
+                            onClick={() => {
+                                if (checked) {
+                                    let arr = JSON.parse(localStorage.record)
+                                    arr.push({ click: 'play' })
+                                    localStorage.record = JSON.stringify(arr)
+                                }
+                                setMode('play')
+                            }
+                            }
 
-                    >
-                        <PlayCircleFilledWhiteOutlinedIcon />
-                    </IconButton>
-                    <IconButton
-                        className={classes.play}
-                        name="stop"
-                        onClick={() => setMode('stop')}
-                        color="secondary"
-                    >
-                        <StopOutlinedIcon />
-                    </IconButton>
-                </ButtonGroup>
-            </div>
-        </Paper>
-    </ThemeProvider>
+                        >
+                            <PlayCircleFilledWhiteOutlinedIcon />
+                        </IconButton>
+                        <IconButton
+                            className={classes.play}
+                            name="stop"
+                            onClick={() => {
+                                if (checked) {
+                                    let arr = JSON.parse(localStorage.record)
+                                    arr.push({ click: 'stop', time: Date.now() })
+                                    localStorage.record = JSON.stringify(arr)
+                                }
+                                setMode('stop')
+                            }
+                            }
+                            color="secondary"
+                        >
+                            <StopOutlinedIcon />
+                        </IconButton>
+                    </ButtonGroup>
 
-}
+                    {error ? <Typography variant="h5" color="error">{error}</Typography> : ''}
+                    <Typography className={classes.playRecord}>play & record</Typography>
+                    <Switch
+                        checked={checked}
+                        onChange={handleRec}
+                        inputProps={{ 'aria-label': 'controlled' }}
+                    />
+                    {session ?
+                        <Button
+                            className={classes.session}
+                            onClick={playSession}
+                        >PLAY SESSION</Button> : ''}
+                </div>
+            </Paper>
+        </ThemeProvider>
+
+    }
